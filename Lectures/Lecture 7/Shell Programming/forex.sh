fd165e5a9575112be5abaa1045a13ce2657aa6c4
abhirupcse <121986277+abhirupcse@users.noreply.github.com>
2023-08-18 02:10:31 -0400

    Add files via upload

---
+++ Lectures/Lecture 7/Shell Programming/forex.sh
@@ -0,0 +1,29 @@
+
+#!/bin/bash
+
+echo For loop with an explicit list 
+
+for i in 2 4 6 8 15
+do
+echo $i
+done
+
+
+
+echo For Loop with range and default increment of 1
+
+for i in {1..10}
+do
+echo $i
+done
+
+
+echo For loop with increments of 2 
+
+for i in {1..10..2}
+do
+echo $i
+done
+
+
+
